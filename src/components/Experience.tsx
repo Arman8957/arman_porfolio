dd22163d59d088a3f150246be04b3061e062d73f
Arman8957 <novicethought0@gmail.com>
2025-01-23 04:00:43 +0600

    project link added

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -9,35 +9,38 @@ export function AnimatedPinDemo() {
     <div className="h-[40rem] w-full flex items-center justify-center ">
         
       <PinContainer
-        title="/ui.aceternity.com"
-        href="https://twitter.com/mannupaaji"
+        title="/supervisorsolutions.com"
+        href="https://supervisor-solution-comapany-format.vercel.app/coursesPages"
       >
         <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
           <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-            Aceternity UI
+           Supervisor Solutions
           </h3>
           <div className="text-base !m-0 !p-0 font-normal">
             <span className="text-slate-500 ">
-              Customizable Tailwind CSS and Framer Motion Components.
+            JavaScript, TypeScript, 
+            Redux, Node.js, Express.js, Mongodb, Axios
             </span>
           </div>
           <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
         </div>
       </PinContainer>
       <PinContainer
-        title="/ui.aceternity.com"
-        href="https://twitter.com/mannupaaji"
+        title="/teachertoday.com"
+        href="https://teachertoday.com"
       >
         <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
           <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-            Aceternity UI
+            Teacher Today
           </h3>
           <div className="text-base !m-0 !p-0 font-normal">
             <span className="text-slate-500 ">
-              Customizable Tailwind CSS and Framer Motion Components.
+            JavaScript, TypeScript,Tailwindcss, Material UI,
+            Redux, Node.js, Express.js, No-Sql(Mongodb), SWR, Culinary for storage.
             </span>
           </div>
           <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+          <img />
         </div>
       </PinContainer>
     </div>
