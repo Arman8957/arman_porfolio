3159a5ba12b4233302ea14f7bd3b3cc588427008
Arman8957 <novicethought0@gmail.com>
2025-01-23 02:10:10 +0600

    portfolio added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,99 @@
-import type { NextConfig } from "next";
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'assets.aceternity.com',
+//         pathname: '**', 
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'pbs.twimg.com', 
+//         pathname: '**',
+//       },
+//     ],
+//   },
+// };
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// module.exports = nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//     images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'pbs.twimg.com',
+//         pathname: '**', 
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'assets.aceternity.com',
+//         pathname: '**', 
+//       },
+//     ],
+//   },
+  /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: false,
+  output: 'standalone', 
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', 
+        hostname: 'pbs.twimg.com', 
+        pathname: '**',
+      },
+      {
+                protocol: 'https',
+                hostname: 'assets.aceternity.com',
+                pathname: '**', 
+      },
+    ],
+  },
+
 };
 
-export default nextConfig;
+
+// module.exports = nextConfig;
+
+
+  // images: {
+  //   domains: ['pbs.twimg.com', 'assets.aceternity.com'], // Add external image domains here
+  // },
+// };
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'pbs.twimg.com',
+//         pathname: '**', 
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'assets.aceternity.com',
+//         pathname: '**', 
+//       },
+//     ],
+//   },
+
+// };
+
+// export default nextConfig;
+
+// // images: {
+// //   remotePatterns: [
+// //     {
+// //       protocol: 'https',
+// //       hostname: 'example.com',
+// //     },
+// //     {
+// //       protocol: 'https',
+// //       hostname: 'cdn.example.com',
+// //     },
+// //   ],
---
+++ src/components/Hero.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BackgroundLines } from "@/components/ui/background-lines";
+import {ColourfulText} from "@/components/ui/colourful-text";
+import { NavbarDemo } from "./Navbar";
+
+
+export function BackgroundLinesDemo() {
+  return (
+    <BackgroundLines className="flex items-center justify-center w-full flex-col sm:py-4 px-4">
+      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+        Hi, <br />This is  Md Arman.
+      </h2>
+      <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+      "Committed to creating <ColourfulText text="responsive, scalable, and high-performance" /> web applications that empower businesses, foster growth, and deliver outstanding user experiences."
+      </p>
+    
+      <h1 className="text-2xl md:text-5xl lg:text-7xl font-bold text-center text-white relative z-2 font-sans">
+        I am a <ColourfulText text="Full-Stack Web App And Android developer" /> <br /> you will ever find
+      </h1>
+  
+      <NavbarDemo />
+    </BackgroundLines>
+  );
+}
