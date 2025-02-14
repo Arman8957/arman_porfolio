f39a602c5d196952306197d0f23202c0d99664f9
Arman8957 <novicethought0@gmail.com>
2025-02-14 10:37:26 +0600

    card added

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -34,7 +34,7 @@ export function AnimatedPinDemo() {
             Teacher Today
           </h3>
           <div className="text-base !m-0 !p-0 font-normal">
-            <span className="text-slate-500 ">
+            <span className="text-white">
             JavaScript, TypeScript,Tailwindcss, Material UI,
             Redux, Node.js, Express.js, No-Sql(Mongodb), SWR, Culinary for storage.
             </span>
@@ -43,6 +43,42 @@ export function AnimatedPinDemo() {
           <img />
         </div>
       </PinContainer>
+      <PinContainer
+        title="/librarybookshelf.org"
+        href="https://library-book-shelf.vercel.app/"
+      >
+        <div className="flex basis-full flex-col p-2 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+            Teacher Today
+          </h3>
+          <div className="text-base !m-0 !p-0 font-normal">
+            <span className="text-white ">
+            JavaScript, TypeScript,Tailwindcss, Redix UI
+            Contextapi Node.js, Nest.js, postgreSQL, SWR, Socket.io
+            </span>
+          </div>
+          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+          <img />
+        </div>
+      </PinContainer>
+      {/* <PinContainer
+        title="/teacherstoday.org"
+        href="https://teacherstoday.org/"
+      >
+        <div className="flex basis-full flex-col p-2 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+            Teacher Today
+          </h3>
+          <div className="text-base !m-0 !p-0 font-normal">
+            <span className="text-slate-500 ">
+            JavaScript, TypeScript,Tailwindcss, Material UI,
+            Redux, Node.js, Express.js, No-Sql(Mongodb), SWR, Culinary for storage.
+            </span>
+          </div>
+          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+          <img />
+        </div>
+      </PinContainer> */}
     </div>
    
   );
